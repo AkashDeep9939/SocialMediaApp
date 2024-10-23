7907af11c29843a4cb6eb8d06a09a788f97f016a
Akash Deep <akashdeep9939@gmail.com>
2024-10-23 19:45:49 +0530

    Added users from dummyapi as well

--- SocialMediaApp/src/component/MainContainer/MainContainer.jsx
+++ SocialMediaApp/src/component/MainContainer/MainContainer.jsx
@@ -1,34 +1,22 @@
-import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
-import PostCard from '../PostCard';
-
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+import PostCardList from '../PostCardList/PostCardList';
+import UserList from '../UserList/UserList';
 
 export default function RowAndColumnSpacing() {
   return (
-    <Box display="flex" justifyContent="center" sx={{ width: '100%', mt: '3rem' }}>
+    <Box display="flex" justifyContent="space-around" sx={{ width: '100%', mt: '3rem' }}>
       <Grid container alignItems="start" justifyContent="center" rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-        <Grid xs={0} md={3}>
-          <Item>1</Item>
+        <Grid  md={6}>
+        <UserList />
         </Grid>
         <Grid 
             container 
             alignItems="center"
             justifyContent="center"
-            direction="column" xs={12} md={9}>
+            direction="column" md={6}>
             {/* Posts */}
-          <PostCard />
-          <PostCard />
-          <PostCard />
-          <PostCard />
+            <PostCardList />
         </Grid>
       </Grid>
     </Box>
